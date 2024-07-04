9fabc58fa5ae37a002b5d78dc5a2a7f8f0df81c8
Sylvain UTARD <sylvain.utard@sorare.com>
2024-07-04 10:58:58 +0200

    Add -r/--rules support
    I stumbled upon this tool and I believe it's the easiest to use by far
    to test the validity of a query based on a schema.
    
    Unfortunately, my need was especially about spotting deprecated fields
    usage and I realized this wasn't supported quite yet.
    
    Let me know what you think about this.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-#!/usr/bin/env node
 import { Command } from 'commander'
 import { Source } from '@graphql-tools/utils'
-import { GraphQLError, SourceLocation, validate } from 'graphql'
+import {
+  GraphQLError,
+  SourceLocation,
+  validate,
+  specifiedRules,
+  ValidationRule,
+  NoDeprecatedCustomRule,
+} from 'graphql'
 import { loadDocuments, loadSchema } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { JsonFileLoader } from '@graphql-tools/json-file-loader'
@@ -11,40 +17,106 @@ import chalk from 'chalk'
 import { version } from '../package.json'
 import terminalLink from 'terminal-link'
 
-
-const GRAPHQL_ERROR_GUIDE_LINKS_MAP: [rule_message_regex: RegExp, link: string][] = [
-  [/Cannot query field "(.*)" on type "(.*)"/, 'https://thegraph.com/docs/en/#UnknownX'],
+const GRAPHQL_ERROR_GUIDE_LINKS_MAP: [
+  rule_message_regex: RegExp,
+  link: string,
+][] = [
+  [
+    /Cannot query field "(.*)" on type "(.*)"/,
+    'https://thegraph.com/docs/en/#UnknownX',
+  ],
   [/Unknown directive "(.*)"/, 'https://thegraph.com/docs/en/#UnknownX'],
   [/Unknown fragment "(.*)"/, 'https://thegraph.com/docs/en/#UnknownX'],
   [/Unknown type "(.*)"/, 'https://thegraph.com/docs/en/#UnknownX'],
   [/Variable "(.*)" is not defined/, 'https://thegraph.com/docs/en/#UnknownX'],
-  [/This anonymous operation must be the only defined operation/, 'https://thegraph.com/docs/en/#LoneAnonymousOperationRule'],
-  [/Fragment "(.*)" is never used/, 'https://thegraph.com/docs/en/#NoUnusedFragmentsRule'],
-  [/Variable "(.*)" is never used/, 'https://thegraph.com/docs/en/#NoUnusedVariablesRule'],
-  [/Fields "(.*)" conflict because (.*)\. Use different aliases on the fields to fetch both if this was intentional\./, 'https://thegraph.com/docs/en/#OverlappingFieldsCanBeMergedRule'],
-  [/Fragment cannot condition on non composite type "(.*)"/, 'https://thegraph.com/docs/en/#FragmentsOnCompositeTypesRule'],
-  [/Fragment cannot be spread here as objects/, 'https://thegraph.com/docs/en/#PossibleFragmentSpreadsRule'],
-  [/Field "(.*)" must not have a selection since type "(.*)" has no subfields./, 'https://thegraph.com/docs/en/#ScalarLeafsRule'],
-  [/Field "(.*)" of type "(.*)" must have a selection of subfields. Did you mean "(.*) { ... }"/, 'https://thegraph.com/docs/en/#ScalarLeafsRule'],
-  [/Argument "(.*)" can only be defined once/, 'https://thegraph.com/docs/en/#UniqueArgument'],
-  [/There can be only one argument named "(.*)"/, 'https://thegraph.com/docs/en/#UniqueArgument'],
-  [/Directive "(.*)" may not be used on (.*)\./, 'https://thegraph.com/docs/en/#KnownDirectivesRule'],
-  [/The directive "(.*)" can only be used once at this location/, 'https://thegraph.com/docs/en/#UniqueDirectivesPerLocationRule'],
-  [/There can be only one fragment named "(.*)"/, 'https://thegraph.com/docs/en/#UniqueFragmentNamesRule'],
-  [/There can be only one operation named "(.*)"/, 'https://thegraph.com/docs/en/#UniqueOperationNamesRule'],
-  [/There can be only one variable named "(.*)"/, 'https://thegraph.com/docs/en/#UniqueVariableNamesRule'],
-  [/Variable "(.*)" of type "(.*)" used in position expecting type "(.*)"/, 'https://thegraph.com/docs/en/#VariablesInAllowedPositionRule'],
+  [
+    /This anonymous operation must be the only defined operation/,
+    'https://thegraph.com/docs/en/#LoneAnonymousOperationRule',
+  ],
+  [
+    /Fragment "(.*)" is never used/,
+    'https://thegraph.com/docs/en/#NoUnusedFragmentsRule',
+  ],
+  [
+    /Variable "(.*)" is never used/,
+    'https://thegraph.com/docs/en/#NoUnusedVariablesRule',
+  ],
+  [
+    /Fields "(.*)" conflict because (.*)\. Use different aliases on the fields to fetch both if this was intentional\./,
+    'https://thegraph.com/docs/en/#OverlappingFieldsCanBeMergedRule',
+  ],
+  [
+    /Fragment cannot condition on non composite type "(.*)"/,
+    'https://thegraph.com/docs/en/#FragmentsOnCompositeTypesRule',
+  ],
+  [
+    /Fragment cannot be spread here as objects/,
+    'https://thegraph.com/docs/en/#PossibleFragmentSpreadsRule',
+  ],
+  [
+    /Field "(.*)" must not have a selection since type "(.*)" has no subfields./,
+    'https://thegraph.com/docs/en/#ScalarLeafsRule',
+  ],
+  [
+    /Field "(.*)" of type "(.*)" must have a selection of subfields. Did you mean "(.*) { ... }"/,
+    'https://thegraph.com/docs/en/#ScalarLeafsRule',
+  ],
+  [
+    /Argument "(.*)" can only be defined once/,
+    'https://thegraph.com/docs/en/#UniqueArgument',
+  ],
+  [
+    /There can be only one argument named "(.*)"/,
+    'https://thegraph.com/docs/en/#UniqueArgument',
+  ],
+  [
+    /Directive "(.*)" may not be used on (.*)\./,
+    'https://thegraph.com/docs/en/#KnownDirectivesRule',
+  ],
+  [
+    /The directive "(.*)" can only be used once at this location/,
+    'https://thegraph.com/docs/en/#UniqueDirectivesPerLocationRule',
+  ],
+  [
+    /There can be only one fragment named "(.*)"/,
+    'https://thegraph.com/docs/en/#UniqueFragmentNamesRule',
+  ],
+  [
+    /There can be only one operation named "(.*)"/,
+    'https://thegraph.com/docs/en/#UniqueOperationNamesRule',
+  ],
+  [
+    /There can be only one variable named "(.*)"/,
+    'https://thegraph.com/docs/en/#UniqueVariableNamesRule',
+  ],
+  [
+    /Variable "(.*)" of type "(.*)" used in position expecting type "(.*)"/,
+    'https://thegraph.com/docs/en/#VariablesInAllowedPositionRule',
+  ],
 ]
 
-const logError = (document: Source, location: SourceLocation, error: GraphQLError): void => {
-  const errorGuideLink = GRAPHQL_ERROR_GUIDE_LINKS_MAP.find(([regexp]) => regexp.test(error.message))
+const logError = (
+  document: Source,
+  location: SourceLocation,
+  error: GraphQLError,
+): void => {
+  const errorGuideLink = GRAPHQL_ERROR_GUIDE_LINKS_MAP.find(([regexp]) =>
+    regexp.test(error.message),
+  )
 
   const str = `${chalk.red(error.message)}
   ${chalk.yellow(document.location)}${chalk.yellow('#')}${chalk.yellow(
     location.line,
   )}:${chalk.yellow(location.column)}
-  ${errorGuideLink ? `\n More information at ${terminalLink(errorGuideLink[1], errorGuideLink[1], { fallback: false })}` : ''}
-          `
+  ${
+    errorGuideLink
+      ? `\n More information at ${terminalLink(
+          errorGuideLink[1],
+          errorGuideLink[1],
+          { fallback: false },
+        )}`
+      : ''
+  }`
 
   log(str)
 }
@@ -58,7 +130,11 @@ const program = new Command()
  * @param {string} schemaPath
  * @param {string} documentPath
  */
-const validateOperations = async (schemaPath: string, documentPath: string) => {
+const validateOperations = async (
+  schemaPath: string,
+  documentPath: string,
+  rules: ValidationRule[],
+) => {
   // This allows us to load schema from a remote URL or from a local file
   const schema = await loadSchema(schemaPath, {
     loaders: [new UrlLoader(), new GraphQLFileLoader(), new JsonFileLoader()],
@@ -70,7 +146,7 @@ const validateOperations = async (schemaPath: string, documentPath: string) => {
   })
 
   documents.forEach((document) => {
-    const errors = validate(schema, document.document!)
+    const errors = validate(schema, document.document!, rules)
 
     if (errors.length <= 0) {
       log(
@@ -87,6 +163,8 @@ const validateOperations = async (schemaPath: string, documentPath: string) => {
   })
 }
 
+const defaultRules = specifiedRules.concat(NoDeprecatedCustomRule)
+
 const main = async () => {
   program
     .name('validate-operations')
@@ -101,12 +179,21 @@ const main = async () => {
       'Path to the operation files',
       '**/*.graphql',
     )
+    .option(
+      '-r, --rules <Rule1,Rule2,...,RuleN>',
+      'The list of rules to apply for the validation',
+    )
     .parse(process.argv)
 
   const schema = program.opts().schema
   const operation = program.opts().operation
+  const rules = (program.opts().rules || '').split(',').filter(Boolean)
 
-  await validateOperations(schema, operation)
+  await validateOperations(
+    schema,
+    operation,
+    defaultRules.filter((f) => rules.length === 0 || rules.includes(f.name)),
+  )
 }
 
 main().catch((e) => console.error(e))
